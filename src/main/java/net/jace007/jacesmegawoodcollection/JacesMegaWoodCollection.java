2f04ff14f05fa7000ccb5a7af6ea743244f38b95
iix-Jace007 <juliaarinsberg07@gmail.com>
2025-12-28 18:57:24 -0500

    Add Mod Items Class

--- src/main/java/net/jace007/jacesmegawoodcollection/JacesMegaWoodCollection.java
+++ src/main/java/net/jace007/jacesmegawoodcollection/JacesMegaWoodCollection.java
@@ -1,5 +1,6 @@
 package net.jace007.jacesmegawoodcollection;
 
+import net.jace007.jacesmegawoodcollection.item.JMWCItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -50,6 +51,8 @@ public class JacesMegaWoodCollection {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        JMWCItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
---
+++ src/main/java/net/jace007/jacesmegawoodcollection/item/JMWCItems.java
@@ -0,0 +1,15 @@
+package net.jace007.jacesmegawoodcollection.item;
+
+import net.jace007.jacesmegawoodcollection.JacesMegaWoodCollection;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class JMWCItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(JacesMegaWoodCollection.MOD_ID);
+
+
+
+    public static void register (IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
