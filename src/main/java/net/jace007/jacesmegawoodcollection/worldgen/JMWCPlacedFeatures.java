40e072bbb2aa640e41e2be8ae2d1f1f0c9508fc9
iix-Jace007 <juliaarinsberg07@gmail.com>
2025-12-28 19:57:36 -0500

    added Worldgen

---
+++ src/main/java/net/jace007/jacesmegawoodcollection/worldgen/JMWCPlacedFeatures.java
@@ -0,0 +1,31 @@
+package net.jace007.jacesmegawoodcollection.worldgen;
+
+import net.jace007.jacesmegawoodcollection.JacesMegaWoodCollection;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+
+import java.util.List;
+
+public class JMWCPlacedFeatures {
+
+    public static void bootstrap(BootstrapContext<PlacedFeature> context) {
+        var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
+
+    }
+
+    private static ResourceKey<PlacedFeature> registerKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, ResourceLocation.fromNamespaceAndPath(JacesMegaWoodCollection.MOD_ID, name));
+    }
+
+    private static void register(BootstrapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration,
+                                 List<PlacementModifier> modifiers) {
+        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
+    }
+}
